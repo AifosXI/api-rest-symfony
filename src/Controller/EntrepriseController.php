c5a758c7955b576ee68b3515bfcf2194d7b9babf
Kbakazami <struys.wyvin@gmail.com>
2023-11-07 10:25:28 +0100

    Entreprise controller
    Add: Controller entreprise & view

---
+++ src/Controller/EntrepriseController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Contracts\HttpClient\HttpClientInterface;
+
+class EntrepriseController extends AbstractController
+{
+
+    private HttpClientInterface $client;
+
+    public function __construct(HttpClientInterface $client) {
+        $this->client = $client;
+    }
+
+    #[Route('/search-company', name: 'search_company', methods: ['GET'])]
+    public function getCompanyByName(Request $request) {
+
+        $param = $request->get('company_name');
+
+        $companiesJson = $this->client->request(
+            'GET',
+            'https://recherche-entreprises.api.gouv.fr/search?q=' . $param
+        );
+
+        $companies = json_decode($companiesJson->getContent());
+
+
+        return $this->render('entreprise/index.html.twig', [
+            'companies' => $companies,
+        ]);
+    }
+}
--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -3,44 +3,17 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\Serializer\SerializerInterface;
-use Symfony\Contracts\HttpClient\HttpClientInterface;
-use const http\Client\Curl\Features\HTTP2;
 
 class HomeController extends AbstractController
 {
-
-    private HttpClientInterface $client;
-
-    public function __construct(HttpClientInterface $client) {
-        $this->client = $client;
-    }
 
     #[Route('/', name: 'app_home')]
     public function index(): Response
     {
         return $this->render('home/index.html.twig', [
             'controller_name' => 'HomeController',
-        ]);
-    }
-
-    #[Route('/search-company', name: 'search_company')]
-    public function getCompanyByName(SerializerInterface $serializer) {
-        $company = $this->client->request(
-            'GET',
-            'https://recherche-entreprises.api.gouv.fr/search?q=' . $_GET['company_name']
-        );
-
-        var_dump($company->getContent());
-
-//        $companyJson = $serializer->serialize($company,'json');
-//        return new JsonResponse($companyJson, 200, [], true);
-
-        return $this->render('home/index.html.twig', [
-            'company' => $company->getContent(),
         ]);
     }
 }
