2d88f1ba57a6525fdcdbdc3ca08a170eb168227a
Sofia Chaudhry <sofia.chaudhry2606@gmail.com>
2023-11-24 11:10:29 +0100

    feature/3 - Exercice 3
    Added content type verification

--- public/ApiOuverteEntListe.php
+++ public/ApiOuverteEntListe.php
@@ -4,6 +4,9 @@ namespace public;
 
 var_dump($_SERVER['REQUEST_METHOD']);
 var_dump($_SERVER['HTTP_ACCEPT']);
+var_dump($_SERVER['CONTENT_TYPE']);
+//var_dump($_SERVER);
+var_dump($_GET);
 
 if($_SERVER['REQUEST_METHOD'] === 'GET')
 {
@@ -29,9 +32,31 @@ if($_SERVER['REQUEST_METHOD'] === 'GET')
             }
         }
         var_dump($companiesFiles);
+        if($companiesFiles)
+        {
+            if($_SERVER['CONTENT_TYPE'] === 'application/json')
+            {
+                $json = json_encode($companiesFiles);
+                echo 'Liste des entreprises présentes sous format json <br/>';
+                echo $json;
+                return http_response_code(200);
+            }
+            else if ($_SERVER['CONTENT_TYPE'] === 'text/csv')
+            {
+                var_dump('csv format');
+            }
+            else {
+                echo 'Format non prit en compte';
+                return http_response_code(406);
+            }
+        }
         return $companiesFiles;
+    } else {
+        echo 'Aucune entreprise enregistrée';
+        return http_response_code(200);
     }
 } else {
+    echo 'Mauvais "verbe utilisé"';
     return http_response_code(405);
 }
 
